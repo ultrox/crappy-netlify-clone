184d35dc8e2f483efcc5024c5535788c40988114
Marko <crashxx@gmail.com>
2019-08-23 17:40:56 +0200

    add details to site information

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,23 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtils = {
+        '.rotate-10deg': {
+          transform: 'rotate(-10deg)',
+          position: 'absolute',
+        },
+        '.rotate-15deg': {
+          transform: 'rotate(-15deg)',
+          position: 'absolute',
+        },
+        '.rotate-40deg': {
+          transform: 'rotate(-40deg)',
+          position: 'absolute',
+        },
+      }
+      addUtilities(newUtils)
+    },
+  ],
 }
